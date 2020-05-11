34521bce972279be2b9e33e77623a8d6d2e640f0
cawauchi6204 <cawauchi6204@gmail.com>
2020-05-11 21:43:48 +0900

    組み込みオブジェクトのお勉強

--- js/chap03/scripts/sort_clazz.js
+++ js/chap03/scripts/sort_clazz.js
@@ -1,11 +1,25 @@
-var classes = ['部長', '課長', '主任', '担当'];
+// var classes = ['部長', '課長', '主任', '担当'];
+// var members = [
+//   { name: '鈴木清子', clazz: '主任' },
+//   { name: '山口久雄', clazz: '部長' },
+//   { name: '井上太郎', clazz: '担当' },
+//   { name: '和田知美', clazz: '課長' },
+//   { name: '小森雄太', clazz: '担当' },
+// ];
+// console.log(members.sort(function(x, y) {
+//   return classes.indexOf(x.clazz) - classes.indexOf(y.clazz);
+// }))
+
+var classes = ['部長' , '課長' , '主任' , '担当'];
+
 var members = [
-  { name: '鈴木清子', clazz: '主任' },
-  { name: '山口久雄', clazz: '部長' },
-  { name: '井上太郎', clazz: '担当' },
-  { name: '和田知美', clazz: '課長' },
-  { name: '小森雄太', clazz: '担当' },
+  { name: '担当', clazz:'担当' },
+  { name: '課長', clazz:'課長' },
+  { name: '主任', clazz:'主任' },
+  { name: '担当', clazz:'担当' },
+  { name: '部長', clazz:'部長' },
 ];
-console.log(members.sort(function(x, y) {
-  return classes.indexOf(x.clazz) - classes.indexOf(y.clazz);
+
+console.log(members.sort(( x , y ) => {
+  return classes.indexOf(x.clazz)-classes.indexOf(y.clazz)
 }))
